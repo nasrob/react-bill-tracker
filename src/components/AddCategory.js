f1c44b6a7022b72d971b564e85cd316be6767dde
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-07-29 19:29:02 +0200

    added styling to addCategory component

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -19,15 +19,29 @@ const AddCategory = (props) => {
 	};
 
 	return (
-		<form>
-			<h1>Enter a category of bills</h1>
-			<p>E.g. 'Electricity' or 'Gas' or 'Internet'</p>
-			<input
-				placeholder="Add a category"
-				value={category}
-				onChange={handleChange}
-			/>
-			<button onClick={handleSubmit}>Add</button>
+		<form className="h-100 w-full flex items-center justify-center font-sans">
+			<div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+				<div className="mb-4">
+					<h1 className="text-grey-darkest">
+						Enter a category of bills
+					</h1>
+					<p>E.g. 'Electricity' or 'Gas' or 'Internet'</p>
+					<div className="flex mt-4">
+						<input
+							className="shadow appearnce-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+							placeholder="Add a category"
+							value={category}
+							onChange={handleChange}
+						/>
+						<button
+							className="flex-no-shrink p-2 border-2 rounded bg-teal text-white border-teal hover:text-white hover:bg-teal"
+							onClick={handleSubmit}
+						>
+							Add
+						</button>
+					</div>
+				</div>
+			</div>
 		</form>
 	);
 };
