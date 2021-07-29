5b2831218b12d7eecbf052df16c83b6e75467cb5
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-07-29 19:14:57 +0200

    implemented addCategory feature

--- src/App.js
+++ src/App.js
@@ -2,19 +2,36 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import BillsTable from "./components/BillsTable";
 import Chart from "./components/Chart";
+import { useState } from "react";
+import AddCategory from "./components/AddCategory";
 
 function App() {
+	const [shouldShowCategory, setShouldShowCategory] = useState(true);
+	const [categories, setCategories] = useState([]);
+
+	const addCategory = (category) => {
+		const updatedCategories = [...(categories || []), category];
+		setCategories(updatedCategories);
+		setShouldShowCategory(false);
+	};
+
 	return (
 		<div className="App">
-			<NavBar />
-			<div className="container flex">
-				<div className="w-1/2">
-					<BillsTable />
+			{shouldShowCategory ? (
+				<AddCategory onSubmit={addCategory} />
+			) : (
+				<div>
+					<NavBar />
+					<div className="container flex">
+						<div className="w-1/2">
+							<BillsTable />
+						</div>
+						<div className="w-1/2">
+							<Chart />
+						</div>
+					</div>
 				</div>
-				<div className="w-1/2">
-					<Chart />
-				</div>
-			</div>
+			)}
 		</div>
 	);
 }
--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,5 +1,35 @@
-const AddCategory = () => {
-	return <div>AddCategory</div>;
+import { useState } from "react";
+
+const AddCategory = (props) => {
+	const [category, setCategory] = useState();
+
+	const handleChange = (event) => {
+		console.log(event.target.value);
+		setCategory(event.target.value);
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		if (!category) {
+			alert("Please Enter a category");
+			return;
+		}
+
+		props.onSubmit(category);
+	};
+
+	return (
+		<form>
+			<h1>Enter a category of bills</h1>
+			<p>E.g. 'Electricity' or 'Gas' or 'Internet'</p>
+			<input
+				placeholder="Add a category"
+				value={category}
+				onChange={handleChange}
+			/>
+			<button onClick={handleSubmit}>Add</button>
+		</form>
+	);
 };
 
 export default AddCategory;
